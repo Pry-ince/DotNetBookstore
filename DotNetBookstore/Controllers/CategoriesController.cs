ae7704be6c846963485a9a27d4fa2aea2ecf7fde
Pry-ince <punprince07@gmail.com>
2025-03-18 11:45:22 -0400

    Week 02 - Completed

---
+++ DotNetBookstore/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using DotNetBookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetBookstore.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var categories = new List<Category>();
+            for (var i = 1; i < 11; i++)
+            {
+                categories.Add(new Category { CategoryId = i, Name = "Category " + i.ToString() });
+            }    
+                return View(categories);
+        }
+
+        public IActionResult Browse(string category)
+        {
+            ViewBag.Category = category;
+            return View();
+        }
+    }
+}
